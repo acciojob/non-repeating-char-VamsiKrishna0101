8adb5f700071fd67dad8727b9ec77f42c2778b8c
VamsiKrishna0101 <153058517+VamsiKrishna0101@users.noreply.github.com>
2025-06-03 16:29:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ function firstNonRepeatedChar(str) {
  // Write your code here
 	let map=new Map()
 for(let value of str){
-    if(map.has(value)){
-        map.set(value,map.get(value)+1)
-    }else{
-        map.set(value,1)
-    }
+ map.set(value,(map.get(value)||0)+1)
 }
 for(let value of str){
     if(map.get(value)==1){
